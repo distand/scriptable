34fb28ffa7744ee3543933677f98f75912d47d46
Distand <distand@outlook.com>
2020-10-10 14:08:49 +0800

    开始脚本

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# scriptable
+# 首先需要安装 [Scriptable](https://scriptable.app)
+
+1. 点击右上角创建脚本，复制start.js的内容至脚本，然后点标题重命名为start，点左上角done
+2. 点start更新脚本
+
--- start.js
+++ start.js
@@ -1,59 +1,37 @@
-const saver = 1     //存储位置1.本机 2.icloud
 const menu_url = 'https://raw.githubusercontent.com/distand/scriptable/main/menu.json'
-
-const fm = saver == 1 ? FileManager.local() : FileManager.iCloud()
-const dict = fm.documentsDirectory()
-
-let confirm = await _alert('确认要更新脚本吗？', ['确认','取消'])
-if (confirm == '确认'){
-    await sync()
-}
-
+const fm = FileManager.iCloud()
+const confirm = await _alert('确认要更新脚本吗？', ['确认','取消'])
+if(confirm === 0) await sync()
 async function sync() {
-    log('更新开始');
     const menu = await _get(menu_url,'json')
     if(!menu){
-        log('获取目录失败')
+        await _alert('获取目录失败', ['确认'])
         return
     }
-    let data
-    menu.forEach((s) => {
-        data = await _get(s.url)
-        _write(s.name, data)
-        log(s.name)
-    });
-    log('更新结束')
+    let succ = 0
+    for(let i=0;i<menu.length;i++){
+        const s = menu[i]
+        const data = await _get(s.url)
+        if(data){
+            _write(s.name, data)
+            succ++
+        }
+    }
+    await _alert('更新了'+succ+'个脚本', ['确认'])
 }
-
-const _get = async (url, type='') => {
-    const request = new Re
+async function _get(url, type=''){
+    const request = new Request('')
     request.url = url
     request.method = 'GET'
     let data
-    switch (type) {
-        case "json":
-            data = request.loadJSON()
-            break
-        case "image":
-            data = request.loadImage()
-            break
-        default:
-            data = request.loadString()
+    if(type == 'json'){
+        data = request.loadJSON()
+    }else{
+        data = request.loadString()
     }
     return data
 }
-
-const _write = (fileName, content) => {
-    let filePath = dict + '/' + initFile(fileName)
-    fm.writeString(filePath, content)
-    return true
-}
-
-const initFile = (fileName) => {
-    return fileName.lastIndexOf('.') < 0 ? fileName + '.js' : fileName
-}
-
-const _alert = async (message, options) => {
+async function _alert(message, options){
     let alert = new Alert()
     alert.message = message
     for (const option of options) {
@@ -61,9 +39,7 @@ const _alert = async (message, options) => {
     }
     return await alert.presentAlert()
 }
-
-module.exports = {
-    _get,
-    _write,
-    _alert,
+function _write(fileName, content){
+    fileName = fileName.lastIndexOf('.') < 0 ? fileName + '.js' : fileName
+    fm.writeString(fm.documentsDirectory() + '/' + fileName, content)
 }
